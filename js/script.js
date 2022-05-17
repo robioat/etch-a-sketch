10bf86becf3b23acc5bdf2a04e6fd9a1d3191702
Rob Oates <roates@gmail.com>
2022-05-17 12:44:10 +0100

    Added Custom Gridsize User Functionality

--- js/script.js
+++ js/script.js
@@ -1,13 +1,31 @@
-const container=document.querySelector('.container');
-for(i=0;i<256;i++){
-    const div=document.createElement('div');
-    container.appendChild(div);
+
+function deleteGrid(container){
+    container.textContent='';
 }
 
-const childDivs=document.querySelectorAll('.container div');
-childDivs.forEach((child) => {
-    child.addEventListener('mouseover', function(e) {
-        e.target.style.background='blue';
-    });
+function drawGrid(gridSize){
+    const container=document.querySelector('.container');
+    container.style.gridTemplateRows=`repeat(${gridSize},50px)`;
+    container.style.gridTemplateColumns=`repeat(${gridSize},50px)`;
+    deleteGrid(container);
+    let totalGrid=gridSize*gridSize;
+    for(i=0;i<totalGrid;i++){
+        const div=document.createElement('div');
+        container.appendChild(div);
+        div.addEventListener('mouseover', function(e) {
+            e.target.style.background='blue';
+        });
+    }
+}
+
+const gridButton=document.querySelector('#grid-size');
+gridButton.addEventListener('click', ()=> {
+    let gridSize=prompt("Please enter the size of the grid (100 maximum)","");
+    if(gridSize>100){
+        gridSize=100;
+    }
+    drawGrid(gridSize);
 });
+
+drawGrid(16);
 
